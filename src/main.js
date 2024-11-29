80420e146366f6a622c43ea8c9055e0978b022ba
Navat2007 <navat2007@yandex.ru>
2024-11-29 16:38:09 +0300

    add

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+
+import './styles/style.css'
 import './styles/fonts.css'
 import './styles/globals.css'
 import './styles/variables.css'
