fc486d7f3981893f3274d70b0954fee277244cb0
Savallera <phenix-vn@yandex.ru>
2024-11-29 16:22:47 +0300

    header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,52 @@
-<script setup>
-
-</script>
+<script setup></script>
 
 <template>
- Header
+  <header class="header">
+    <div class="header__wrap">
+      <div class="header__logo"></div>
+      <menu class="header__menu">
+        <nav class="header__nav">
+          <ul class="header__list">
+            <li><a href="#" class="header__link">Афиша</a></li>
+            <li><a href="#" class="header__link">Спортобъекты</a></li>
+            <li><a href="#" class="header__link">Тренеры</a></li>
+            <li><a href="#" class="header__link">Услуги</a></li>
+            <li><a href="#" class="header__link">Сообщество</a></li>
+          </ul>
+        </nav>
+      </menu>
+      <div class="header__search"></div>
+      <div class="header__burger"></div>
+    </div>
+  </header>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
+.header {
+  &__wrap {
+    inline-size: var(--main-inline-size);
+    max-inline-size: 100%;
+  }
 
+  &__logo {
+  }
+
+  &__menu {
+  }
+
+  &__nav {
+  }
+
+  &__list {
+  }
+
+  &__link {
+  }
+
+  &__search {
+  }
+
+  &__burger {
+  }
+}
 </style>
